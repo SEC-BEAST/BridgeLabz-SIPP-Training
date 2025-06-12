53120578d7700653ef0068b96187b030977ef84d
Ayush Singh <81176819+SEC-BEAST@users.noreply.github.com>
2025-06-12 09:42:44 +0530

    Add files via upload

---
+++ age.java
@@ -0,0 +1,8 @@
+public class age {
+    public static void main(String[] args){
+        int currentyear=2024;
+        int birthyear=2000;
+        int age = currentyear-birthyear;
+        System.out.println("Harry's age in 2024 is:" + age);
+    }
+}
---
+++ sam.java
@@ -0,0 +1,10 @@
+public class sam {
+    public static void main(String[] args){
+        int phy=95;
+        int math=94;
+        int chem=96;
+        int sum=phy+math+chem;
+        int avg = sum/3;
+        System.out.println("Sam’s average mark in PCM is:"+avg);
+    }
+}
