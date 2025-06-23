92e6e68a072eb056df942c18bfb22fd540dc3e87
Ayush Singh <81176819+SEC-BEAST@users.noreply.github.com>
2025-06-23 08:38:35 +0530

    Delete Strings directory

--- Strings/src/Q19.java
+++
@@ -1,8 +0,0 @@
-public class Q19 {
-    public static void main(String[] args) {
-        int[] m = {78, 86, 91};
-        double perc = (m[0] + m[1] + m[2]) / 3.0;
-        char grade = perc >= 90 ? 'A' : perc >= 75 ? 'B' : 'C';
-        System.out.println("Percentage: " + perc + ", Grade: " + grade);
-    }
-}
